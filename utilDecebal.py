8ffa87526e6497ff53a70f6a43236cae15163071
Cecil Westerhof <CLDWesterhof@gmail.com>
2015-04-29 16:50:48 +0200

    Make it work with Python 3

--- utilDecebal.py
+++ utilDecebal.py
@@ -1,5 +1,5 @@
 # This Python file uses the following encoding: utf-8
-"""Some utilty functions that could be handy"""
+"""Some utilty classes and functions that could be handy"""
 
 ##### Classes
 
@@ -32,7 +32,7 @@ class MovingAverage:
         if not isinstance(length, int):
             raise TypeError("Length should be integral")
         if length < 2:
-            raise ValueError, 'Parameter should be greater or equal 2'
+            raise ValueError('Parameter should be greater or equal 2')
         self._length            = length
         self._old_values        = []
         self._current_total     = 0.0
@@ -85,23 +85,23 @@ def memoize(function):
 
 def test_moving_average(length, input_array, output_array):
     if len(input_array) != len(output_array):
-        raise ValueError, \
-            'Arrays have different lengths. Input: {0}, output: {1}.'. \
-            format(len(input_array), len(output_array))
+        raise ValueError(
+            'Arrays have different lengths. Input: {0}, output: {1}.'.
+            format(len(input_array), len(output_array)))
 
     print('Moving Average length {0}:'.format(length))
     average = MovingAverage(length)
     error   = False
     for i in range(length):
         expected    = output_array[i]
-        expected2   = sum(input_array[max(0, i + 1 - length):i + 1]) \
-                      * 1. / min(length, i + 1)
-        gotten      = average.next_value(input_array[i])
+        expected2   = repr(sum(input_array[max(0, i + 1 - length):i + 1]) \
+                      * 1. / min(length, i + 1))
+        gotten      = repr(average.next_value(input_array[i]))
         if gotten != expected2:
             print('Moving average: {0}, slice {1}, for slice {2}'. \
                 format(gotten, expected2, i + 1))
             error = True
-        if str(gotten) != expected:
+        if gotten != expected:
             print('Got {0} instead of {1} for the {2} value'. \
                 format(gotten, expected, i))
             error = True
@@ -122,7 +122,7 @@ if __name__ == '__main__':
         """
 
         if n < 0:
-            raise ValueError, 'Parameter cannot be negative'
+            raise ValueError('Parameter cannot be negative')
         elif (n == 0) or (n == 1):
             return n
         else:
@@ -136,13 +136,15 @@ if __name__ == '__main__':
         """
 
         if n < 0:
-            raise ValueError, 'Parameter cannot be negative'
+            raise ValueError('Parameter cannot be negative')
         elif (n == 0) or (n == 1):
             return n
         else:
             return fibonacci_memoize(n - 1) + fibonacci_memoize(n - 2)
 
-    print('Testing memoize with fibonacci')
+    speedup = 700000
+    print('Testing if fibonacci(40) is at least {0} times faster with memoize'.
+          format(speedup))
     Error = False
     time_fibonacci      = timeit('fibonacci(40)',
                                  setup   = 'from __main__ import fibonacci',
@@ -152,11 +154,8 @@ if __name__ == '__main__':
                                  number  = 1)
     # In my tests mostly above 750000, but build in a safety margin
     # It shows that memoize can be interesting indeed
-    speedup = 700000
     if time_fibonacci / time_fibonacci_mem < speedup:
         print('Looks like memoize does not work correctly')
-        print('fibonacci_memoize should be {0} times faster as fibonacci'. \
-            format(speedup))
         print('It was only {0} times faster ({1} / {2})'. \
             format(time_fibonacci / time_fibonacci_mem, \
                    time_fibonacci, \
@@ -164,30 +163,41 @@ if __name__ == '__main__':
         Error = True
     if not Error:
         print('Memoize OK')
-    print time_fibonacci, time_fibonacci_mem, time_fibonacci / time_fibonacci_mem
-    print
+    print('')
 
     # For testing moving average, if someone wants to give real big sets …
     input06 = [
-        20.0, 22.0, 21.0, 24.0, 24.0, 23.0, 25.0, 26.0, 20.0, 24.0,
-        26.0, 26.0, 25.0, 27.0, 28.0, 27.0, 29.0, 27.0, 25.0, 24.0,
+        20.0, 22.0, 21.0,
+        24.0, 24.0, 23.0,
+        25.0, 26.0, 20.0,
+        24.0, 26.0, 26.0,
+        25.0, 27.0, 28.0,
+        27.0, 29.0, 27.0,
+        25.0, 24.0,
     ]
     output06 = [
-        '20.0',          '21.0',          '21.0',          '21.75',         '22.2',
-        '22.3333333333', '23.1666666667', '23.8333333333', '23.6666666667', '23.6666666667',
-        '24.0',          '24.5',          '24.5',          '24.6666666667', '26.0',
-        '26.5',          '27.0',          '27.1666666667', '27.1666666667', '26.6666666667',
+        '20.0',               '21.0',               '21.0',
+        '21.75',              '22.2',               '22.333333333333332',
+        '23.1666666667',      '23.8333333333',      '23.6666666667',
+        '23.6666666667',      '24.0',               '24.5',
+        '24.5',               '24.6666666667',      '26.0',
+        '26.5',               '27.0',               '27.1666666667',
+        '27.1666666667',      '26.6666666667',
     ]
     input10 = [
-        20.0, 22.0, 24.0, 25.0, 23.0, 26.0, 28.0, 26.0, 29.0, 27.0,
-        28.0, 30.0, 27.0, 29.0, 28.0,
+        20.0, 22.0, 24.0,
+        25.0, 23.0, 26.0,
+        28.0, 26.0, 29.0,
+        27.0, 28.0, 30.0,
+        27.0, 29.0, 28.0,
     ]
     output10 = [
-        '20.0',          '21.0',          '22.0',          '22.75',         '22.8',
-        '23.3333333333', '24.0',          '24.25',         '24.7777777778', '25.0',
-        '25.8',          '26.6',          '26.9',          '27.3',          '27.8',
+        '20.0',               '21.0',               '22.0',
+        '22.75',              '22.8',               '23.333333333333332',
+        '24.0',               '24.25',              '24.77777777777778',
+        '25.0',               '25.8',               '26.6',
+        '26.9',               '27.3',               '27.8',
     ]
-
     test_moving_average( 6, input06, output06)
     test_moving_average(10, input10, output10)
-    print
+    print('')
