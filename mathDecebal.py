5acc57206fde07d667a7e54d539203e9790e2180
Cecil Westerhof <CLDWesterhof@gmail.com>
2015-05-01 18:37:59 +0200

    Added happy numbers, lucky numbers works with Python 3

--- mathDecebal.py
+++ mathDecebal.py
@@ -70,6 +70,50 @@ def fibonacci_old(n):
     else:
         return fibonacci_old(n - 1) + fibonacci_old(n - 2)
 
+def happy_number(n):
+    """
+    Check if a number is a happy number
+    https://en.wikipedia.org/wiki/Happy_number
+    """
+
+    def create_current(n):
+        return int(''.join(sorted(str(n))))
+
+
+    global _happy_set
+    global _unhappy_set
+
+    run     = set()
+    current = create_current(n)
+    while True:
+        if (current in run) or (current in _unhappy_set):
+            _unhappy_set |= run
+            return False
+        if current in _happy_set:
+            _happy_set |= run
+            return True
+        run.add(current)
+        current = create_current(sum([_squares[value] for value in str(current)]))
+
+# By making this a function it is possible to reset the values for testing purposes
+def happy_number_init():
+    global _happy_set
+    global _squares
+    global _unhappy_set
+
+    _happy_set      = { 1 }
+    _unhappy_set    = set()
+    _squares        = dict((str(i), i*i) for i in range(1, 10))
+
+def happy_number_list(n):
+    found = []
+
+    for i in range(1, n + 1):
+        if happy_number(i):
+            found.append(i)
+    return found
+
+happy_number_init()
 
 def lucky_numbers(n):
     """
@@ -79,7 +123,7 @@ def lucky_numbers(n):
 
     if n < 3:
         return [1]
-    sieve = range(1, n + 1, 2)
+    sieve = list(range(1, n + 1, 2))
     sieve_index = 1
     while True:
         sieve_len   = len(sieve)
@@ -88,7 +132,7 @@ def lucky_numbers(n):
         skip_count  = sieve[sieve_index]
         if sieve_len < skip_count:
             break
-        del sieve[skip_count - 1 : (sieve_len // skip_count) * skip_count : skip_count]
+        del sieve[skip_count - 1 : : skip_count]
         sieve_index += 1
     return sieve
 
@@ -206,6 +250,34 @@ if __name__ == '__main__':
         'fibonacci_iterative(500):\n{0}\n'
         'That is why fibonacci calls fibonacci_iterative.'.
         format(fibonacci_iterative(500)))
+    print('')
+
+    print('Testing happy numbers')
+    happy_numbers_list = [
+        1,   7,   10,  13,  19,  23,  28,  31,  32,  44,
+        49,  68,  70,  79,  82,  86,  91,  94,  97,  100,
+        103, 109, 129, 130, 133, 139, 167, 176, 188, 190,
+        192, 193, 203, 208, 219, 226, 230, 236, 239, 262,
+        263, 280, 291, 293, 301, 302, 310, 313, 319, 320,
+        326, 329, 331, 338, 356, 362, 365, 367, 368, 376,
+        379, 383, 386, 391, 392, 397, 404, 409, 440, 446,
+        464, 469, 478, 487, 490, 496, 536, 556, 563, 565,
+        566, 608, 617, 622, 623, 632, 635, 637, 638, 644,
+        649, 653, 655, 656, 665, 671, 673, 680, 683, 694,
+        700, 709, 716, 736, 739, 748, 761, 763, 784, 790,
+        793, 802, 806, 818, 820, 833, 836, 847, 860, 863,
+        874, 881, 888, 899, 901, 904, 907, 910, 912, 913,
+        921, 923, 931, 932, 937, 940, 946, 964, 970, 973,
+        989, 998, 1000
+    ]
+    happy_found = []
+    for i in range(1, 1001):
+        if happy_number(i):
+            happy_found.append(i)
+    if happy_found == happy_numbers_list:
+        print('Happy numbers OK')
+    else:
+        print('ERROR in happy list')
     print('')
 
     print('Testing lucky numbers')
