b78e20fff3f72295a2c4a70f926c4700c78d4af3
Cecil Westerhof <CLDWesterhof@gmail.com>
2015-06-22 23:57:22 +0200

    Added template.py: a template to use when writing a new module.

---
+++ template.py
@@ -0,0 +1,31 @@
+# This Python file uses the following encoding: utf-8
+'''Description of the file'''
+
+from __future__     import division, print_function
+
+# imports
+
+# froms
+
+
+##### Exception Classes
+
+
+##### Classes
+
+
+##### Functions
+
+
+##### Test functions
+
+
+
+##### Init
+
+# variables
+
+# functions
+
+# __main__
+# if __name__ == '__main__':
